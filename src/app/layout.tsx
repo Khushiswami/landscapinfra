56a865b7166d44332a37bd1320ee4e7bad425eb6
Khushiswami <khushipanthi2002@gmail.com>
2025-10-16 18:57:11 +0530

    madhu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
 import { Metadata } from "next";
 import "./globals.css";
-import { Kufam } from "next/font/google";
-
-// Import Kufam font
-const kufam = Kufam({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
   title: "Landsking Infra",
@@ -20,8 +16,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* Apply Kufam as the default font */}
-      <body className={kufam.className}>{children}</body>
+      {/* Apply Arial as the default font */}
+      <body className="font-sans">{children}</body>
     </html>
   );
 }
